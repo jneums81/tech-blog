021d5d8db37eaca4303d75dd3d51e17da2845904
Jackson Neuman <neumanjackson@gmail.com>
2023-12-03 14:29:09 -0600

    fixed seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Post, Comment} = require('../models');
+const { User, Blog, Comment} = require('../models');
 
 const userData = require('./userData.json');
-const postData = require('./postData.json');
+const blogData = require('./blogData.json');
 const commentData =require('./commentData.json')
 
 const seedDatabase = async () => {
@@ -13,9 +13,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const post of postData) {
-    await Post.create({
-      ...post,
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
